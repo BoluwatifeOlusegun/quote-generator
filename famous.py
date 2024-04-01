9d679f6954618dab8ce2c245fe8299583ffc02e1
BoluwatifeOlusegun <boluwatifeolusegun018@gmail.com>
2024-04-01 02:18:34 -0300

    famous.py list has been fixed

--- famous.py
+++ famous.py
@@ -2,8 +2,8 @@
 notables = [
 
 "The only thing we have to fear is fear itself. - Franklin D. Roosevelt",
-"I think, therefore I am." - René Descartes",
-"To be or not to be, that is the question." - William Shakespeare",
+"I think, therefore I am. - René Descartes",
+"To be or not to be, that is the question. - William Shakespeare",
 "In the beginning, God created the heavens and the earth.- The Bible",
 "The only constant in life is change. - Heraclitus",
 "That's one small step for a man, one giant leap for mankind. - Neil Armstrong",
@@ -18,88 +18,89 @@ notables = [
 "It is not the strongest of the species that survive, nor the most intelligent, but the one most responsive to change. - Charles Darwin",
 "The journey of a thousand miles begins with one step. - Lao Tzu",
 "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe. - Albert Einstein",
-"The only thing you can do easily is be wrong, and that's hardly worth the effort." - Norton Juster
-"The greatest glory in living lies not in never falling, but in rising every time we fall." - Nelson Mandela
-"The first step toward success is taken when you refuse to be a captive of the environment in which you first find yourself." - Mark Caine
-"Believe you can and you're halfway there." - Theodore Roosevelt
-"To love and be loved is to feel the sun from both sides." - David Viscott
-"The greatest glory in living lies not in never falling, but in rising every time we fall." - Nelson Mandela
-"The purpose of our lives is to be happy." - Dalai Lama
-"The only limit to our realization of tomorrow will be our doubts of today." - Franklin D. Roosevelt
-"Success is not final, failure is not fatal: It is the courage to continue that counts." - Winston Churchill
-"The only way to do great work is to love what you do." - Steve Jobs
-"The best revenge is to be unlike him who performed the injustice." - Marcus Aurelius
-"The greatest glory in living lies not in never falling, but in rising every time we fall." - Nelson Mandela
-"The purpose of our lives is to be happy." - Dalai Lama
-"The only limit to our realization of tomorrow will be our doubts of today." - Franklin D. Roosevelt
-"Success is not final, failure is not fatal: It is the courage to continue that counts." - Winston Churchill
-"The only way to do great work is to love what you do." - Steve Jobs
-"The best revenge is to be unlike him who performed the injustice." - Marcus Aurelius
-"The only limit to our realization of tomorrow will be our doubts of today." - Franklin D. Roosevelt
-"Success is not final, failure is not fatal: It is the courage to continue that counts." - Winston Churchill
-"The only way to do great work is to love what you do." - Steve Jobs
-"The best revenge is to be unlike him who performed the injustice." - Marcus Aurelius
-"Success is not final, failure is not fatal: It is the courage to continue that counts." - Winston Churchill
-"The only way to do great work is to love what you do." - Steve Jobs
-"The best revenge is to be unlike him who performed the injustice." - Marcus Aurelius
-"Success is not final, failure is not fatal: It is the courage to continue that counts." - Winston Churchill
-"The only way to do great work is to love what you do." - Steve Jobs
-"The best revenge is to be unlike him who performed the injustice." - Marcus Aurelius
-"Success is not final, failure is not fatal: It is the courage to continue that counts." - Winston Churchill
-"The only way to do great work is to love what you do." - Steve Jobs
-"The best revenge is to be unlike him who performed the injustice." - Marcus Aurelius
-"Success is not final, failure is not fatal: It is the courage to continue that counts." - Winston Churchill
-"The only way to do great work is to love what you do." - Steve Jobs
-"The best revenge is to be unlike him who performed the injustice." - Marcus Aurelius
-"Success is not final, failure is not fatal: It is the courage to continue that counts." - Winston Churchill
-"The only way to do great work is to love what you do." - Steve Jobs
-"The best revenge is to be unlike him who performed the injustice." - Marcus Aurelius
-"Success is not final, failure is not fatal: It is the courage to continue that counts." - Winston Churchill
-"The only way to do great work is to love what you do." - Steve Jobs
-"The best revenge is to be unlike him who performed the injustice." - Marcus Aurelius
-"Success is not final, failure is not fatal: It is the courage to continue that counts." - Winston Churchill
-"The only way to do great work is to love what you do." - Steve Jobs
-"The best revenge is to be unlike him who performed the injustice." - Marcus Aurelius
-"Success is not final, failure is not fatal: It is the courage to continue that counts." - Winston Churchill
-"The only way to do great work is to love what you do." - Steve Jobs
-"The best revenge is to be unlike him who performed the injustice." - Marcus Aurelius
-"Success is not final, failure is not fatal: It is the courage to continue that counts." - Winston Churchill
-"The only way to do great work is to love what you do." - Steve Jobs
-"The best revenge is to be unlike him who performed the injustice." - Marcus Aurelius
-"Success is not final, failure is not fatal: It is the courage to continue that counts." - Winston Churchill
-"The only way to do great work is to love what you do." - Steve Jobs
-"The best revenge is to be unlike him who performed the injustice." - Marcus Aurelius
-"Success is not final, failure is not fatal: It is the courage to continue that counts." - Winston Churchill
-"The only way to do great work is to love what you do." - Steve Jobs
-"The best revenge is to be unlike him who performed the injustice." - Marcus Aurelius
-"Success is not final, failure is not fatal: It is the courage to continue that counts." - Winston Churchill
-"The only way to do great work is to love what you do." - Steve Jobs
-"The best revenge is to be unlike him who performed the injustice." - Marcus Aurelius
-"Success is not final, failure is not fatal: It is the courage to continue that counts." - Winston Churchill
-"The only way to do great work is to love what you do." - Steve Jobs
-"The best revenge is to be unlike him who performed the injustice." - Marcus Aurelius
-"Success is not final, failure is not fatal: It is the courage to continue that counts." - Winston Churchill
-"The only way to do great work is to love what you do." - Steve Jobs
-"The best revenge is to be unlike him who performed the injustice." - Marcus Aurelius
-"Success is not final, failure is not fatal: It is the courage to continue that counts." - Winston Churchill
-"The only way to do great work is to love what you do." - Steve Jobs
-"The best revenge is to be unlike him who performed the injustice." - Marcus Aurelius
-"Success is not final, failure is not fatal: It is the courage to continue that counts." - Winston Churchill
-"The only way to do great work is to love what you do." - Steve Jobs
-"The best revenge is to be unlike him who performed the injustice." - Marcus Aurelius
-"Success is not final, failure is not fatal: It is the courage to continue that counts." - Winston Churchill
-"The only way to do great work is to love what you do." - Steve Jobs
-"The best revenge is to be unlike him who performed the injustice." - Marcus Aurelius
-"Success is not final, failure is not fatal: It is the courage to continue that counts." - Winston Churchill
-"The only way to do great work is to love what you do." - Steve Jobs
-"The best revenge is to be unlike him who performed the injustice." - Marcus Aurelius
-"Success is not final, failure is not fatal: It is the courage to continue that counts." - Winston Churchill
-"The only way to do great work is to love what you do." - Steve Jobs
-"The best revenge is to be unlike him who performed the injustice." - Marcus Aurelius
-"Success is not final, failure is not fatal: It is the courage to continue that counts." - Winston Churchill
-"The only way to do great work is to love what you do." - Steve Jobs
-"The best revenge is to be unlike him who performed the injustice." - Marcus Aurelius
-"Success is not final, failure is not fatal: It is the courage to continue that counts." - Winston Churchill
-"The only way to do great work is to love what you do." - Steve Jobs
-
+"The only thing you can do easily is be wrong, and that's hardly worth the effort. - Norton Juster",
+"The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela",
+"The first step toward success is taken when you refuse to be a captive of the environment in which you first find yourself. - Mark Caine",
+"Believe you can and you're halfway there.- Theodore Roosevelt",
+"To love and be loved is to feel the sun from both sides. - David Viscott",
+"The greatest glory in living lies not in never falling, but in rising every time we fall.- Nelson Mandela",
+"The purpose of our lives is to be happy. - Dalai Lama",
+"The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt",
+"Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+"The only way to do great work is to love what you do. - Steve Jobs",
+"The best revenge is to be unlike him who performed the injustice. - Marcus Aurelius",
+"The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela",
+"The purpose of our lives is to be happy. - Dalai Lama",
+"The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt",
+"Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+"The only way to do great work is to love what you do. - Steve Jobs",
+"The best revenge is to be unlike him who performed the injustice. - Marcus Aurelius",
+"The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt",
+"Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+"The only way to do great work is to love what you do. - Steve Jobs",
+"The best revenge is to be unlike him who performed the injustice. - Marcus Aurelius",
+"Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+"The only way to do great work is to love what you do. - Steve Jobs",
+"The best revenge is to be unlike him who performed the injustice. - Marcus Aurelius",
+"Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+"The only way to do great work is to love what you do. - Steve Jobs",
+"The best revenge is to be unlike him who performed the injustice. - Marcus Aurelius",
+"Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+"The only way to do great work is to love what you do. - Steve Jobs",
+"The best revenge is to be unlike him who performed the injustice. - Marcus Aurelius",
+"Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+"The only way to do great work is to love what you do. - Steve Jobs",
+"The best revenge is to be unlike him who performed the injustice. - Marcus Aurelius",
+"Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+"The only way to do great work is to love what you do. - Steve Jobs",
+"The best revenge is to be unlike him who performed the injustice. - Marcus Aurelius",
+"Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+"The only way to do great work is to love what you do. - Steve Jobs",
+"The best revenge is to be unlike him who performed the injustice.  - Marcus Aurelius",
+"Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+"The only way to do great work is to love what you do.  - Steve Jobs",
+"The best revenge is to be unlike him who performed the injustice. - Marcus Aurelius",
+"Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+"The only way to do great work is to love what you do. - Steve Jobs",
+"The best revenge is to be unlike him who performed the injustice. - Marcus Aurelius",
+"Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+"The only way to do great work is to love what you do. - Steve Jobs",
+"The best revenge is to be unlike him who performed the injustice. - Marcus Aurelius",
+"Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+"The only way to do great work is to love what you do. - Steve Jobs",
+"The best revenge is to be unlike him who performed the injustice. - Marcus Aurelius",
+"Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+"The only way to do great work is to love what you do. - Steve Jobs",
+"The best revenge is to be unlike him who performed the injustice. - Marcus Aurelius",
+"Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+"The only way to do great work is to love what you do. - Steve Jobs",
+"The best revenge is to be unlike him who performed the injustice. - Marcus Aurelius",
+"Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+"The only way to do great work is to love what you do. - Steve Jobs",
+"The best revenge is to be unlike him who performed the injustice. - Marcus Aurelius",
+"Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+"The only way to do great work is to love what you do. - Steve Jobs",
+"The best revenge is to be unlike him who performed the injustice. - Marcus Aurelius",
+"Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+"The only way to do great work is to love what you do. - Steve Jobs",
+"The best revenge is to be unlike him who performed the injustice. - Marcus Aurelius",
+"Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+"The only way to do great work is to love what you do. - Steve Jobs",
+"The best revenge is to be unlike him who performed the injustice. - Marcus Aurelius",
+"Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+"The only way to do great work is to love what you do. - Steve Jobs",
+"The best revenge is to be unlike him who performed the injustice. - Marcus Aurelius",
+"Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+"The only way to do great work is to love what you do. - Steve Jobs",
+"The best revenge is to be unlike him who performed the injustice. - Marcus Aurelius",
+"Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+"The only way to do great work is to love what you do. - Steve Jobs",
+"The best revenge is to be unlike him who performed the injustice. - Marcus Aurelius",
+"Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+"The only way to do great work is to love what you do.- Steve Jobs",
+"The best revenge is to be unlike him who performed the injustice. - Marcus Aurelius",
+"Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+"The only way to do great work is to love what you do. - Steve Jobs"
 ]
+
+print(len(notables))
